2cbec3b1bea0356845529a65a5b6ccf4d6686c7b
Frida Bailleres <105307730+FridaBG@users.noreply.github.com>
2024-10-17 08:16:00 +0200

    add assets

---
+++ Card Game/Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Frida Bailleres González on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card Game/Card GameTests/Card_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameTests.swift
+//  Card GameTests
+//
+//  Created by Frida Bailleres González on 17/10/24.
+//
+
+import Testing
+@testable import Card_Game
+
+struct Card_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
